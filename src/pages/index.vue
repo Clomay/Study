609ebf01e96ab2a1ffd30d653c8878d210fa0a3f
Xi miin hoon <77636777+Clomay@users.noreply.github.com>
2021-04-15 01:59:21 +0700

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,20 +3,20 @@
       <div class="info">
         <div class="container">
           <div class="title index-page__title">
-            <h1>Roobee Farm</h1>
+            <h1>PIG Farm</h1>
             <div class="index-page__title-logo">
-              <img src="/pics/roobee-new-year.png" alt="Roobee Farm V2">
+              <img src="/pics/PIG-new-year.png" alt="PIG Farm V2">
             </div>
           </div>
           <div class="balance">
             <div class="balance__inner">
-              <Card title="bROOBEE/BNB" farm-link="/farming_pancake" top-icon="/pics/pancake.png" currency-icon="/pics/bnb.png" daily-pool="25 000" :finished="true" />
-              <Card title="bROOBEE/BNB" farm-link="/farming_bnb" top-icon="/pics/burger.png" currency-icon="/pics/bnb.png" daily-pool="25 000" />
-              <Card title="bROOBEE/BURGER" farm-link="/farming_burger" top-icon="/pics/burger.png" currency-icon="/pics/burger.png" daily-pool="25 000" />
+              <Card title="PIG/BNB" farm-link="/farming_pancake" top-icon="/pics/pancake.png" currency-icon="/pics/bnb.png" daily-pool="25 000" :finished="true" />
+              <Card title="PIG/BNB" farm-link="/farming_bnb" top-icon="/pics/burger.png" currency-icon="/pics/bnb.png" daily-pool="25 000" />
+              <Card title="PIG/BURGER" farm-link="/farming_burger" top-icon="/pics/burger.png" currency-icon="/pics/burger.png" daily-pool="25 000" />
             </div>
             <div class="balance__info">
               <div class="balance__icon">!</div>
-              <div class="balance__name">Pro Tip</div> - Check Roobee.Finance on <a href="https://bscscan.com/yieldfarms" target="_blank">https://bscscan.com/yieldfarms</a>
+              <div class="balance__name">Pro Tip</div> - Check PIG.Finance on <a href="https://bscscan.com/yieldfarms" target="_blank">https://bscscan.com/yieldfarms</a>
             </div>
           </div>
         </div>
