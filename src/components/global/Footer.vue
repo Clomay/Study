5e98ac12f496f8b801863f2e75a7d45fa2f871f0
Xi miin hoon <77636777+Clomay@users.noreply.github.com>
2021-04-15 07:51:15 +0700

    Update Footer.vue

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -1,16 +1,22 @@
 <template>
-  <footer class="footer">
-    <div class="container">
-      <div class="footer__inner">
-        <a target="_blank" href="https://www.bscscan.com/address/0xe64f5cb844946c1f102bd25bbd87a5ab4ae89fbe" class="footer__link">ROOBEE Contract</a>
-        <a target="_blank" href="https://exchange.pancakeswap.finance/#/add/0xE64F5Cb844946C1F102Bd25bBD87a5aB4aE89Fbe/ETH" class="footer__link">PancakeSwap</a>
-        <a target="_blank" href="https://burgerswap.org/?type=1&from=BNB&to=bROOBEE" class="footer__link">BurgerSwap</a>
-        <a target="_blank" href="https://github.com/roobee-platform" class="footer__link">Github</a>
-        <a target="_blank" href="https://t.me/roobee_invest" class="footer__link">Telegram</a>
-        <a target="_blank" href="https://medium.com/@roobee_invest" class="footer__link">Medium</a>
-        <a target="_blank" href="https://twitter.com/Roobee_invest" class="footer__link">Twitter</a>
-        <a target="_blank" href="https://roobee.gitbook.io/roobee-eng/" class="footer__link">Gitbook</a>
-      </div>
+  <!-- Footer -->
+  <mdb-footer color="blue-grey" class="page-footer font-small lighten-5 pt-0">
+    <div style="backgroundColor: '#21d192'">
+      <mdb-container class="text-left">
+        <mdb-row class="py-4 d-flex align-items-center">
+          <mdb-col md="6" lg="5" class="text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+          </mdb-col>
+          <mdb-col md="6" lg="7" class="text-center text-md-right">
+            <a class="fb-ic ml-0"><i class="fab fa-facebook white-text mr-lg-4"> </i></a>
+            <a class="tw-ic"><i class="fab fa-twitter white-text mr-lg-4"> </i></a>
+            <a class="gplus-ic"><i class="fab fa-google-plus white-text mr-lg-4"> </i></a>
+            <a class="li-ic"><i class="fab fa-linkedin-in white-text mr-lg-4"> </i></a>
+            <a class="ins-ic"><i class="fab fa-instagram white-text mr-lg-4"> </i></a>
+          </mdb-col>
+        </mdb-row>
+      </mdb-container>
+
     </div>
   </footer>
 </template>
