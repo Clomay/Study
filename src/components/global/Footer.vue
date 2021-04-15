0e97dd70a2fad5dbeed8b35c2c84d11d7c0b908b
Xi miin hoon <77636777+Clomay@users.noreply.github.com>
2021-04-15 08:07:45 +0700

    Update Footer.vue

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -1,25 +1,71 @@
 <template>
   <!-- Footer -->
-  <mdb-footer color="unique-color-dark" class="page-footer font-small pt-0">
-    <div style="backgroundColor: #6351ce">
-      <mdb-container class="text-left">
-        <mdb-row class="py-4 d-flex align-items-center">
-          <mdb-col md="6" lg="5" class="text-center text-md-left mb-4 mb-md-0">
-            <h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
-          </mdb-col>
-          <mdb-col md="6" lg="7" class="text-center text-md-right">
-            <a class="fb-ic ml-0"><i class="fab fa-facebook white-text mr-lg-4"> </i></a>
-            <a class="tw-ic"><i class="fab fa-twitter white-text mr-lg-4"> </i></a>
-            <a class="gplus-ic"><i class="fab fa-google-plus white-text mr-lg-4"> </i></a>
-            <a class="li-ic"><i class="fab fa-linkedin-in white-text mr-lg-4"> </i></a>
-            <a class="ins-ic"><i class="fab fa-instagram white-text mr-lg-4"> </i></a>
-          </mdb-col>
-        </mdb-row>
+  <mdb-footer color="stylish-color-dark" class="page-footer font-small pt-4 mt-4">
+    <mdb-container class="text-left">
+      <mdb-row>
+        <mdb-col md="6">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
+          <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit.</p>
+        </mdb-col>
+        <hr class="clearfix w-100 d-md-none" />
+        <mdb-col md="2">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </mdb-col>
+        <hr class="clearfix w-100 d-md-none" />
+        <mdb-col md="2">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </mdb-col>
+        <hr class="clearfix w-100 d-md-none" />
+        <mdb-col md="2">
+          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+          <ul class="list-unstyled">
+            <li><a href="#!">Link 1</a></li>
+            <li><a href="#!">Link 2</a></li>
+            <li><a href="#!">Link 3</a></li>
+            <li><a href="#!">Link 4</a></li>
+          </ul>
+        </mdb-col>
+      </mdb-row>
+    </mdb-container>
+    <hr />
+    <div class="text-center py-3">
+      <ul class="list-unstyled list-inline mb-0">
+        <li class="list-inline-item">
+          <h5 class="mb-1">Register for free</h5>
+        </li>
+        <li class="list-inline-item"><a href="#" class="btn btn-danger btn-rounded">Sign up!</a></li>
+      </ul>
+    </div>
+    <hr />
+    <div class="text-center">
+      <ul class="list-unstyled list-inline">
+        <li class="list-inline-item"><a class="btn-floating btn-sm btn-fb mx-1"><i class="fab fa-facebook"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-sm btn-tw mx-1"><i class="fab fa-twitter"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-sm btn-gplus mx-1"><i class="fab fa-google-plus"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-sm btn-li mx-1"><i class="fab fa-linkedin-in"> </i></a></li>
+        <li class="list-inline-item"><a class="btn-floating btn-sm btn-dribbble mx-1"><i class="fab fa-dribbble"> </i></a></li>
+      </ul>
+    </div>
+    <div class="footer-copyright text-center py-3">
+      <mdb-container fluid>
+        &copy; 2020 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
       </mdb-container>
     </div>
-      </div>
-    </div>
-  </footer>
+  </mdb-footer>
+  <!-- Footer -->
 </template>
 
 <script>
